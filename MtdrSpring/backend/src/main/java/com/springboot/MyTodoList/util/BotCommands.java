675bf60403d6cba19716eec14e0d34d3ccdf7e17
GPat0 <gael.pato04@gmail.com>
2024-10-31 18:46:14 -0600

    Botones Telegram
    Algunos funcionan no todos pero no se porque me da muchos errores al momento de intentar hacerlos funcionar y la verdad no se cual es el error

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/util/BotCommands.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/util/BotCommands.java
@@ -4,16 +4,13 @@ public enum BotCommands {
 
     START_COMMAND("/start"),
     HIDE_COMMAND("/hide"),
-
     TODO_LIST("/todolist"), 
     ADD_ITEM("/additem"), 
-
     CREATE_PROJECT("/createproject"),
     LIST_PROJECTS("/listprojects"),
     UPDATE_PROJECT("/updateproject"),
     DELETE_PROJECT("/deleteproject"),
     GET_PROJECT("/getproject"),
-
     LIST_TASKS("/listtasks"),
     CREATE_TASK("/createtask"),
     UPDATE_TASK("/updatetask"),
